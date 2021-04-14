178f8188cadbcc8a897ecf78c5edb12cf2e47b67
Mrfingerstyle <FE_liuyoung@163.com>
2021-04-14 19:40:16 +0800

    add task-079

---
+++ task-079-Vue全解-表单与v-model/src/MyInput.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="red wrapper">
+        <input :value="value" @input="$emit('input', $event.target.value)">
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MyInput',
+    props: {
+        value: {
+            type: String
+        }
+    }
+}
+</script>
+<style>
+    .red {
+        color: red;
+    }
+    .wrapper {
+        display: inline-block;
+    }
+</style>
